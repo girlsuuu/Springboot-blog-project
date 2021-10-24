1bf5efe79458109554d3a804a5c76f4afa404ff0
lixinjian <963597661@qq.com>
2021-10-25 02:25:17 +0800

    project initialization

---
+++ src/main/java/com/blog/SpringbootBlogProjectApplication.java
@@ -0,0 +1,13 @@
+package com.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootBlogProjectApplication.class, args);
+  }
+
+}
