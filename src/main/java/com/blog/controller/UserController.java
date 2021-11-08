f9aa732782436b6dff80d930a96c6e6573c85747
lixinjian <963597661@qq.com>
2021-11-08 23:50:05 +0800

    实体校验及跨域

--- src/main/java/com/blog/controller/UserController.java
+++ src/main/java/com/blog/controller/UserController.java
@@ -7,7 +7,10 @@ import com.blog.service.UserService;
 import javax.jws.soap.SOAPBinding.Use;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +34,12 @@ public class UserController {
   @GetMapping("/index")
   public Object index(){
     User user = userService.getById(1L);
+
+    return Result.success(user);
+  }
+
+  @PostMapping("/save")
+  public Object save(@Validated @RequestBody User user){
 
     return Result.success(user);
   }
--- src/main/java/com/blog/entity/User.java
+++ src/main/java/com/blog/entity/User.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.time.LocalDateTime;
 import java.io.Serializable;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -28,10 +30,13 @@ public class User implements Serializable {
     @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
+    @NotNull(message = "昵称不能为空")
     private String username;
 
     private String avatar;
 
+    @NotNull(message = "邮箱不能为空")
+    @Email(message = "邮箱格式不正确")
     private String email;
 
     private String password;
